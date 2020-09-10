4d2254225bc61dc69ebfb0bf44656276db9cd8a5
Clifton Rawlings <Clifton.Raw@gmail.com>
2020-09-10 02:51:17 -0700

    Shortened code for format

--- src/format.cpp
+++ src/format.cpp
@@ -12,10 +12,13 @@ using std::string;
 string Format::ElapsedTime(long seconds) { 
     std::ostringstream ostream;
 
-    int hours = seconds / 3600;
-    seconds = seconds % 3600;
-    int minutes = seconds / 60;
-    seconds = seconds % 60;
+    int hours;
+    int minutes;
+
+    hours = seconds / 3600;
+    seconds %= 3600;
+    minutes = seconds / 60;
+    seconds %= 60;
 
     ostream << std::setw(2) << std::setfill('0') << hours << ":"
             << std::setw(2) << std::setfill('0') << minutes << ":"
